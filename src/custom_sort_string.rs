dfb4b77ebe7490b5f3e19e90588649f5aefbcc9a
Aniket Prajapati <42654595+aniketfuryrocks@users.noreply.github.com>
2021-07-15 06:09:23 +0530

    moved to new file

---
+++ src/custom_sort_string.rs
@@ -0,0 +1,35 @@
+use std::{cmp::Ordering, collections::HashMap};
+/// 1. order has length at most 26, and no character is repeated in order.
+/// 2. str has length at most 200.
+/// 3. order and str consist of lowercase letters only.
+#[allow(dead_code)]
+fn custom_sort_string(order: String, str: String) -> String {
+    // get chars
+    let mut order_map:HashMap<char, usize> = HashMap::with_capacity(order.len());
+    
+    order.chars().enumerate().for_each(|(c , i)| { order_map.insert(i,c); });
+
+    let mut str_chars:Vec<char> = str.chars().collect();
+
+    str_chars.sort_by(| a, b | {
+        match ( order_map.get(a), order_map.get(b) ) {
+            ( Some(a), Some (b) ) => a.cmp(b) ,
+            ( Some(_), None ) => Ordering::Less ,
+            _ => Ordering::Greater 
+        }
+    });
+    // convert str_chars back to string
+    str_chars.into_iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::custom_sort_string;
+
+    #[test]
+    fn check() {
+        assert_eq!(custom_sort_string("cba".into(), "abcd".into()), "cbad");
+        assert_eq!(custom_sort_string("exa".into(), "xwvee".into()), "eexwv");
+    }
+}
+
